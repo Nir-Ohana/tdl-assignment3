5783fc4ba5a076e291f950d738563b1cd97b38d8
Nir <speednir123@gmail.com>
2020-12-22 14:04:44 +0200

    :[

--- copy_task_base.py
+++ copy_task_base.py
@@ -163,7 +163,7 @@ class Model(nn.Module):
         self.loss_func = nn.CrossEntropyLoss()
 
     def forward(self, inputs):
-        state = torch.zeros(inputs.size(0), self.k, requires_grad=False).cuda()
+        state = torch.zeros(inputs.size(0), self.k, requires_grad=False).cuda(device)
 
         outputs = []
         for input in torch.unbind(inputs, dim=1):
@@ -182,7 +182,7 @@ class Model(nn.Module):
         return self.loss_func(logits.view(-1, 9), y.view(-1))
 
 
-T = 8
+T = 10
 K = 3
 
 batch_size = 128
@@ -193,6 +193,17 @@ hidden_size = 64
 n_characters = n_classes + 1
 lr = 1e-3
 print_every = 100
+
+
+def evaluate(model, test_x, test_y, criterion=nn.CrossEntropyLoss):
+    with torch.no_grad():
+        out = model(test_x)
+        loss = model.loss(out, test_y)
+        pred = out.view(-1, n_classes).data.max(1, keepdim=True)[1]
+        correct = pred.eq(test_y.data.view_as(pred)).cpu().sum()
+        counter = out.view(-1, n_classes).size(0)
+        print('\nTest set: Average loss: {:.8f}  |  Accuracy: {:.4f}\n'.format(
+            loss.item(), 100. * correct / counter))
 
 
 def main():
@@ -206,9 +217,11 @@ def main():
     print('{}, {}'.format(X.shape, Y.shape))
     plt.imshow(X[:3])
     plt.show()
-    X = X.cuda()
-    Y = Y.cuda()
-    ohX = torch.FloatTensor(batch_size, T + 2 * K, n_characters).cuda()
+    plt.imshow(Y[:3])
+    plt.show()
+    X = X.cuda(device)
+    Y = Y.cuda(device)
+    ohX = torch.FloatTensor(batch_size, T + 2 * K, n_characters).cuda(device)
     onehot(ohX, X[:batch_size])
     # print(ohX)
     # print('{}, {}'.format(X[:batch_size].shape, ohX.shape))
@@ -230,19 +243,21 @@ def main():
     opt_LSTM = torch.optim.RMSprop(model_LSTM.parameters(), lr=lr)
 
     print("Baseline Loss {:.3f}".format(cross_entropy_formula(1)))
-
+    correct_mlp = 0
+    correct_rnn = 0
+    correct_lstm = 0
     t_0 = time()
     for step in range(iter):
         # xs.append(step)
         # ys.append(cross_entropy_formula(step))
 
         xs = np.append(xs, step)
-        ys = np.append(ys, cross_entropy_formula(T+K-1))
+        ys = np.append(ys, cross_entropy_formula(T))
         bX = X[step * batch_size: (step + 1) * batch_size]
         bY = Y[step * batch_size: (step + 1) * batch_size]
 
         onehot(ohX, bX)
-
+        # evaluate(model_MLP, ohX, bY)
         opt_MLP.zero_grad()
         opt_RNN.zero_grad()
         opt_LSTM.zero_grad()
@@ -250,8 +265,8 @@ def main():
         logits_RNN = model_RNN(ohX)
         logits_LSTM = model_LSTM(ohX)
         loss_MLP = model_MLP.loss(logits_MLP, bY)
-        loss_RNN = model_MLP.loss(logits_RNN, bY)
-        loss_LSTM = model_MLP.loss(logits_LSTM, bY)
+        loss_RNN = model_RNN.loss(logits_RNN, bY)
+        loss_LSTM = model_LSTM.loss(logits_LSTM, bY)
         loss_MLP.backward()
         loss_RNN.backward()
         loss_LSTM.backward()
@@ -266,16 +281,59 @@ def main():
         if step % print_every == 0:
             print('Step={}, Loss={:.4f}'.format(step, loss_MLP.item()))
 
+            # print(logits_MLP)
+            # print(logits_MLP.shape)
+            # torch.set_printoptions(profile="full")
+            # print(logits_MLP.data)
+            # print(logits_MLP.data.shape)
+            # print(logits_MLP.data[0])
+            # print(torch.argmax(logits_MLP.data[0], dim=1, keepdim=False))
+            # print(logits_MLP.data[0].shape)
+            # _, predicted = torch.max(logits_MLP.data, 2)
+            # print(predicted)
+            # print(predicted.shape)
+            predicted_mlp = torch.argmax(logits_MLP, dim=2, keepdim=False)
+            predicted_rnn = torch.argmax(logits_RNN, dim=2, keepdim=False)
+            predicted_lstm = torch.argmax(logits_LSTM, dim=2, keepdim=False)
+
+            correct_mlp = (predicted_mlp == bY).sum().item()
+            print(correct_mlp)
+            correct_rnn = (predicted_rnn == bY).sum().item()
+            correct_lstm = (predicted_lstm == bY).sum().item()
+            print("MLP accuracy is : {}".format(100 * correct_mlp / (batch_size * 16)))
+            print("RNN accuracy is : {}".format(100 * correct_rnn / (batch_size * 16)))
+            print("LSTM accuracy is : {}".format(100 * correct_lstm / (batch_size * 16)))
+            # print(torch.max(logits_MLP, dim=2, keepdim=False).indices.shape)
+            # print(torch.max(logits_RNN, dim=1, keepdim=False).values)
+            # print(torch.max(logits_MLP, dim=2, keepdim=True).indices.reshape(batch_size, T + 2 * K).shape)
+            # print(torch.max(logits_MLP, dim=2, keepdim=True).values.reshape(batch_size, T + 2 * K).shape)
+            # print(bY)
+            # print(bY.shape)
+        if step == 4999:
+            # print(logits_MLP)
+            # print(logits_MLP.shape)
+            # print(torch.round(torch.max(logits_MLP, dim=2, keepdim=True).values).reshape(128, 16))
+            # print(bX.view(-1, n_classes).data.max(1, keepdim=True)[1])
+            # print(bY)
+            # print(torch.max(logits_MLP, dim=2, keepdim=True).values.reshape(128, 16).shape)
+            # print(torch.round(torch.max(logits_MLP, dim=2, keepdim=True).values))
+            # print(torch.max(logits_MLP, dim=2, keepdim=True).values.shape)
+
+            print(logits_MLP.shape)
+
     t_1 = time()
+    ohX = torch.FloatTensor(batch_size, T + 2 * K, n_characters).cuda(device)
+    onehot(ohX, X[:128])
+    evaluate(model_MLP, ohX, Y[:128])
     print("training took : {:.3f}".format(t_1 - t_0))
     plt.plot(xs, ys, '--g', label='Baseline')
     plt.plot(xs, ys_loss_mlp, '-m', label='MLP')
     plt.plot(xs, ys_loss_rnn, '-y', label='RNN')
     plt.plot(xs, ys_loss_lstm, '-c', label='LSTM')
-    plt.title('{} Time lag'.format(T + K - 1))
+    plt.title('{} Time lag'.format(T))
     plt.legend()
-    plt.xlabel('Training steps')
-    plt.ylabel('Loss')
+    plt.xlabel('Training examples')
+    plt.ylabel('Cross entropy')
     plt.show()
 
 
